a7bae20a562ee0ed56c75086989e87d6d9bf963a
jatj <jatinder01.singh@outlook.comm>
2023-02-16 19:56:13 +0000

    corrected the  tag

--- main.tf
+++ main.tf
@@ -130,7 +130,9 @@ resource "azurerm_virtual_machine" "catapp" {
 
   }
 
-  tags = {"devops"}
+  tags = {
+    Department = "devops"
+    }
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
