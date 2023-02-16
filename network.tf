117569328fb3f870df6303d1e82984b42daffe99
jatj <jatinder01.singh@outlook.comm>
2023-02-16 20:28:27 +0000

    Added network module-3

--- network.tf
+++ network.tf
@@ -1,5 +1,7 @@
 module "network" {
   source  = "app.terraform.io/jatj/network/azurerm"
   version = "3.5.0"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   # insert required variables here
 }
+
